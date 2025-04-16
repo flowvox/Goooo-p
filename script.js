a92cc14f7ea581048d83c2a6841900e06ef60cc2
vijagovindBiju <vijaygovindbiju@gmail.com>
2025-04-16 18:16:41 +0530

    wd

--- script.js
+++ script.js
@@ -8,11 +8,13 @@ let man = document.getElementById("man")
 
 window.addEventListener("scroll" ,()=>{
     let value = window.scrollY;
-    moon.style.top = value * 1.2 + "px";
-    text.style.top = 80 + value * -0.2 +"%"
-    train.style.left = value *3 +"px"
-    d_moon.style.top=80+value*.1 +"px"
-    man.style.left=value*0.4+"px"
+    moon.style.top = value * 1.1 + "px";
+    moon.style.left = value * 1.6 + "px";
+    text.style.top = 80 + value * -0.2 +"%";
+    train.style.left = value *3 +"px";
+    d_moon.style.left = value * 16 + "px";
+    d_moon.style.left=0+value*0.2 +"px";
+    man.style.right =-value*0.4+"px";
 
 }
 );
