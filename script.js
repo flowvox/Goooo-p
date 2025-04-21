a84381f5144cf3c10a004c37034e85c08a4fcfc9
flowvox <christowrites@gmail.com>
2025-04-21 00:02:04 +0000

    added qns to script file

--- script.js
+++ script.js
@@ -19,3 +19,43 @@ window.addEventListener("scroll" ,()=>{
 }
 );
 
+// Reccomendation system logic
+
+const questions = [
+{
+    question:"Who are you going with?",
+    answers:[
+        {text: "Solo", value: "solo"},
+        {text: "Family", value: "family"},
+        {text: "Group", value: "group"},
+    ]
+},
+{
+    question:"Which region would you like to visit?",
+    answers:[
+        {text: "Beaches (Kovalam, Varkala, Marari, Bekal, Cherai)", value: "beach"},
+        {text: "Hill Stations (Munnar, Thekkady, Wayanad, Vagamon)", value: "hill"},
+        {text: "Backwaters (Alleppey, Kumarakom, Kottayam, Kuttanad)", value: "backwater"},
+    ]
+},
+{
+    question:"What’s your budget per person, per day?",
+    answers:[
+        {text: "Budget (up to ₹3 000)", value: "low"},
+        {text: "Moderate (₹3 000–7 000)", value: "medium"},
+        {text: "Luxury (above ₹7 000)", value: "high"},
+    ]
+},
+{
+    question:"How many days will you stay?",
+    answers:[
+        {text: "Short (2–3 days)", value: "three"},
+        {text: "Medium (4–6 days)", value: "six"},
+        {text: "Long (7+ days)", value: "seven"},
+    ]
+},
+]
+
+const questionContainer = document.getElementById("question")
+const answerButtons = document.getElementById("answer-buttons")
+const nextButton = document.getElementById("next-btn")
